8f59706ce7029fb2888fdfbf14dc137029f46491
sasha <sashakozak1996@gmail.com>
2023-10-12 17:52:39 +0300

    fix (update, delete): error if the "where" condition is not specified

--- src/Query/Grammar.php
+++ src/Query/Grammar.php
@@ -2,10 +2,22 @@
 
 namespace Pelfox\LaravelBigQuery\Query;
 
+use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Query\Grammars\Grammar as BaseGrammar;
 
 class Grammar extends BaseGrammar
 {
+    protected $operators = [
+        '+', '-', '~', '*', '/', '||', '<<', '>>', '&', '^', '|', '=', '<', '>',
+        '<=', '>=', '!=', '<>', 'like', 'not like', 'not between', 'between',
+        'not in', 'in', 'is not null', 'is null', 'is not true', 'is true',
+        'is not false', 'is false', 'not', 'and', 'or'
+    ];
+
+    protected $bitwiseOperators = [
+        '~', '&', '|', '<<', '>>', '<<=', '>>=', '^'
+    ];
+
     protected string $tableSuffix = '';
 
     public function setSuffixTable($suffix): static
@@ -33,5 +45,24 @@ class Grammar extends BaseGrammar
         }
 
         return $this->wrap("{$dataset}{$this->tablePrefix}{$table}{$this->tableSuffix}", true);
+    }
+
+    public function compileDelete(Builder $query)
+    {
+        $this->setDefaultWheres($query);
+        return parent::compileDelete($query);
+    }
+
+    protected function setDefaultWheres(Builder $query)
+    {
+        if (!$query->wheres) {
+            $query->whereRaw('1 = 1');
+        }
+    }
+
+    public function compileUpdate(Builder $query, array $values)
+    {
+        $this->setDefaultWheres($query);
+        return parent::compileUpdate($query, $values);
     }
 }
