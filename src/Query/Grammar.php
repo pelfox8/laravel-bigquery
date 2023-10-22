c68d8da165cd07c526151836c3aff6fb0fd5bb52
pelfox <sashakozak1996@gmail.com>
2023-10-22 14:29:55 +0300

    feat: added support methods for work with json

--- src/Query/Grammar.php
+++ src/Query/Grammar.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Query\Grammars\Grammar as BaseGrammar;
 
 class Grammar extends BaseGrammar
 {
+    private string $deleteValue = '_delete_binding_item_';
+
     protected $operators = [
         '+', '-', '~', '*', '/', '||', '<<', '>>', '&', '^', '|', '=', '<', '>',
         '<=', '>=', '!=', '<>', 'like', 'not like', 'not between', 'between',
@@ -64,5 +66,65 @@ class Grammar extends BaseGrammar
     {
         $this->setDefaultWheres($query);
         return parent::compileUpdate($query, $values);
+    }
+
+    protected function wrapJsonSelector($value)
+    {
+        [$field, $path] = $this->wrapJsonFieldAndPath($value);
+
+        return "json_value($field $path)";
+    }
+
+    protected function whereJsonContains(Builder $query, $where)
+    {
+        $not = $where['not'] ? 'not ' : '';
+
+        $this->removeWhereBinging($query);
+
+        return $not . $this->compileJsonContains(
+                $where['column'],
+                $where['value']
+            );
+    }
+
+    protected function compileJsonContains($column, $value)
+    {
+        [$field, $path] = $this->wrapJsonFieldAndPath($column);
+
+        $items = [];
+        foreach ((array)$value as $item) {
+            $item = is_null($item) ? 'null' : '"' . str_replace('"', '\"', $item) . '"';
+            $items[] = "$item in unnest(json_value_array($field$path))";
+        }
+
+        return "(" . implode(' or ', $items) . ")";
+    }
+
+    protected function removeWhereBinging(Builder $query): void
+    {
+        foreach ($query->bindings['where'] as $index => $binding) {
+            if ($binding === $this->deleteValue) {
+                unset($query->bindings['where'][$index]);
+            }
+        }
+    }
+
+    protected function compileJsonLength($column, $operator, $value): string
+    {
+        [$field, $path] = $this->wrapJsonFieldAndPath($column);
+
+        return "array_length(json_value_array($field$path)) $operator $value ";
+    }
+
+    public function prepareBindingForJsonContains($binding): string
+    {
+        return $this->deleteValue;
+    }
+
+    protected function compileJsonContainsKey($column): string
+    {
+        [$field, $path] = $this->wrapJsonFieldAndPath($column);
+
+        return "json_extract_scalar($field$path) is not null";
     }
 }
